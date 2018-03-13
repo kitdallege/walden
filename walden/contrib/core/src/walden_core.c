bee5bdcc2b7589456962df738e066a1fc3a5a3ad
kitdallege <kit.dallege@gmail.com>
2018-03-13 11:37:29 -0500

    some reformatting of c sources.

--- walden/contrib/core/src/walden_core.c
+++ walden/contrib/core/src/walden_core.c
@@ -1,5 +1,5 @@
-#include <stdio.h>                                                                                                                                                                                                  
-#include <stdlib.h>                                                                                                                                                                                                 
+#include <stdio.h>
+#include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
 
@@ -15,8 +15,8 @@ PG_FUNCTION_INFO_V1(slugify);
 extern int is_skip_char(const char *ch);
 
 inline int is_skip_char(const char *ch) {
-    return (*ch == ' ' || *ch == '_' || *ch == '-' || iscntrl(*ch));
- }
+	return (*ch == ' ' || *ch == '_' || *ch == '-' || iscntrl(*ch));
+}
 
 
 /*
@@ -27,39 +27,39 @@ inline int is_skip_char(const char *ch) {
 Datum
 slugify(PG_FUNCTION_ARGS)
 {
-    int first = 1;
-    int needs_hyphen = 0;
-    char *ch;
-    char *src;
-    char *buffer;
-    char *dest;
-    text *src_txt; 
-    text *dest_txt;
+	int first = 1;
+    	int needs_hyphen = 0;
+	char *ch;
+	char *src;
+	char *buffer;
+    	char *dest;
+    	text *src_txt; 
+    	text *dest_txt;
  
-    src_txt = PG_GETARG_TEXT_P(0);
-    src = text_to_cstring(src_txt); 
+    	src_txt = PG_GETARG_TEXT_P(0);
+    	src = text_to_cstring(src_txt); 
     
-    dest = palloc(strlen(src) + 1);
-    buffer = dest;
-    ch = src;
-    while (*ch) {
-        if (isalnum(*ch)) {
-            if (needs_hyphen) {
-                *buffer++ = '-';
-                needs_hyphen = 0;
-            }
-            *buffer++ = tolower(*ch++);
-            first = 0;
-        } else if (is_skip_char(ch))  {
-            while (*ch && is_skip_char(ch)){ch++;}
-            needs_hyphen = first ? 0:1;
-        } else {
-            ch++; 
-        }
-    }
-    *buffer = 0x0;
-    dest_txt = cstring_to_text(dest);
-    pfree(dest);
-    PG_RETURN_TEXT_P(dest_txt);
+    	dest = palloc(strlen(src) + 1);
+    	buffer = dest;
+    	ch = src;
+    	while (*ch) {
+		if (isalnum(*ch)) {
+	    		if (needs_hyphen) {
+				*buffer++ = '-';
+				needs_hyphen = 0;
+			}
+		*buffer++ = tolower(*ch++);
+		first = 0;
+		} else if (is_skip_char(ch))  {
+		    while (*ch && is_skip_char(ch)){ch++;}
+		    needs_hyphen = first ? 0:1;
+		} else {
+		    ch++; 
+		}
+	}
+	*buffer = 0x0;
+	dest_txt = cstring_to_text(dest);
+	pfree(dest);
+	PG_RETURN_TEXT_P(dest_txt);
 } 
 
--- walden/contrib/webdev/src/pgstach.c
+++ walden/contrib/webdev/src/pgstach.c
@@ -1,5 +1,5 @@
-#include <stdio.h>                                                                                                                                                                                                  
-#include <stdlib.h>                                                                                                                                                                                                 
+#include <stdio.h>																								    
+#include <stdlib.h>																								    
 #include <ctype.h>
 #include <string.h>
 
@@ -17,28 +17,27 @@ PG_FUNCTION_INFO_V1(render);
 Datum
 render(PG_FUNCTION_ARGS)
 {
-    int rc;
-    text *template_txt;
-    text *json_txt;
-    text *result_txt;
-    const char *template;
-    const char *json_str;
-    char *result;
-    size_t result_len;
-    
-    template_txt = PG_GETARG_TEXT_P(0);
-    json_txt = PG_GETARG_TEXT_P(1);
-    
-    template = text_to_cstring(template_txt);
-    json_str = text_to_cstring(json_txt);
+	int rc;
+	text *template_txt;
+	text *json_txt;
+	text *result_txt;
+	const char *template;
+	const char *json_str;
+	char *result;
+	size_t result_len;
 
-    rc = mustach_json_cstring(template, json_str, &result, &result_len);
-   if (!rc) {
-        result_txt = cstring_to_text(result);
-        PG_RETURN_TEXT_P(result_txt);
-    } else {
-        PG_RETURN_NULL();
-    }
+	template_txt = PG_GETARG_TEXT_P(0);
+	json_txt = PG_GETARG_TEXT_P(1);
+
+	template = text_to_cstring(template_txt);
+	json_str = text_to_cstring(json_txt);
+
+	rc = mustach_json_cstring(template, json_str, &result, &result_len);
+	if (!rc) {
+		result_txt = cstring_to_text(result);
+		PG_RETURN_TEXT_P(result_txt);
+	} else {
+		PG_RETURN_NULL();
+	}
 }
-
 
