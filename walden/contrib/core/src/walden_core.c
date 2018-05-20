935ef2393bac3e4343743696345b223a267474d8
kitdallege <kit.dallege@gmail.com>
2018-05-20 11:24:39 -0500

    some cleanup work.

--- walden/contrib/core/src/walden_core.c
+++ walden/contrib/core/src/walden_core.c
@@ -14,7 +14,8 @@ PG_FUNCTION_INFO_V1(slugify);
 
 extern int is_skip_char(const char *ch);
 
-inline int is_skip_char(const char *ch) {
+inline int is_skip_char(const char *ch)
+{
 	return (*ch == ' ' || *ch == '_' || *ch == '-' || iscntrl(*ch));
 }
 
@@ -28,33 +29,33 @@ Datum
 slugify(PG_FUNCTION_ARGS)
 {
 	int first = 1;
-    	int needs_hyphen = 0;
+	int needs_hyphen = 0;
 	char *ch;
 	char *src;
 	char *buffer;
-    	char *dest;
-    	text *src_txt; 
-    	text *dest_txt;
- 
-    	src_txt = PG_GETARG_TEXT_P(0);
-    	src = text_to_cstring(src_txt); 
-    
-    	dest = palloc(strlen(src) + 1);
-    	buffer = dest;
-    	ch = src;
-    	while (*ch) {
+	char *dest;
+	text *src_txt;
+	text *dest_txt;
+
+	src_txt = PG_GETARG_TEXT_P(0);
+	src = text_to_cstring(src_txt); 
+
+	dest = palloc(strlen(src) + 1);
+	buffer = dest;
+	ch = src;
+	while (*ch) {
 		if (isalnum(*ch)) {
-	    		if (needs_hyphen) {
+			if (needs_hyphen) {
 				*buffer++ = '-';
 				needs_hyphen = 0;
 			}
-		*buffer++ = tolower(*ch++);
-		first = 0;
+			*buffer++ = tolower(*ch++);
+			first = 0;
 		} else if (is_skip_char(ch))  {
-		    while (*ch && is_skip_char(ch)){ch++;}
-		    needs_hyphen = first ? 0:1;
+			while (*ch && is_skip_char(ch)){ch++;}
+			needs_hyphen = first ? 0:1;
 		} else {
-		    ch++; 
+			ch++; 
 		}
 	}
 	*buffer = 0x0;
